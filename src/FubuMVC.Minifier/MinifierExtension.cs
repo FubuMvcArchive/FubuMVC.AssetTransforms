87de94c9e8d9f6ae912876999e135c130f1040a4
Alex Henning Johannessen <ahjohannessen@gmail.com>
2012-01-11 14:45:27 +0000

    Added project for Minifier bottle

---
+++ src/FubuMVC.Minifier/MinifierExtension.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FubuMVC.Core;
+
+namespace FubuMVC.Minifier
+{
+    public class MinifierExtension : IFubuRegistryExtension
+    {
+        public void Configure(FubuRegistry registry)
+        {
+        }
+    }
+}
