0dc4676a604c532d5b5804b308627b5a6b6ad023
Ryan Hansen <ketiko@gmail.com>
2012-01-12 11:56:34 -0700

    Cleaning up test code.

--- src/FubuMVC.Sass.Tests/SassTransformerTester.cs
+++ src/FubuMVC.Sass.Tests/SassTransformerTester.cs
@@ -3,17 +3,23 @@ using NUnit.Framework;
 
 namespace FubuMVC.Sass.Tests
 {
+    [TestFixture]
     public class SassTransformerTester : InteractionContext<SassTransformer>
     {
+        protected override void beforeEach()
+        {
+            var sassCompiler = new SassAndCoffee.Ruby.Sass.SassCompiler();
+            Services.Inject(typeof(ISassCompiler), new DefaultSassCompiler(sassCompiler));
+        }
+
         [Test]
         public void should_compile()
         {
-            Services.Inject(typeof(ISassCompiler), new DefaultSassCompiler(new SassAndCoffee.Ruby.Sass.SassCompiler()));
-
-            var actual = ClassUnderTest.Transform("@mixin mine { color: blue } body { @include mine }", null);
-
             const string expected = "body {\n  color: blue; }\n";
-            actual.ShouldEqual(expected);
+            const string contents = "@mixin mine { color: blue } body { @include mine }";
+
+            ClassUnderTest.Transform(contents, null)
+                .ShouldEqual(expected);
         }
     }
 }
