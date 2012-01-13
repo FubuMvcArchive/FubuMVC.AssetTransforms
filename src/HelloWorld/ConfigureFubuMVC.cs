a8c94d1f881f28926e3f227605ac67e428cd5f9f
Alex Henning Johannessen <ahjohannessen@gmail.com>
2012-01-13 13:31:48 +0000

    Spiced up Less example a bit with less functionality.

--- src/HelloWorld/ConfigureFubuMVC.cs
+++ src/HelloWorld/ConfigureFubuMVC.cs
@@ -1,3 +1,4 @@
+using FubuMVC.Coffee;
 using FubuMVC.Core;
 using FubuMVC.Less;
 using FubuMVC.Sass;
@@ -26,6 +27,8 @@ namespace HelloWorld
 
             Import<LessExtension>();
             Import<SassExtension>();
+            Import<CoffeeExtension>();
+
             Assets.CombineAllUniqueAssetRequests();
 
             this.UseSpark();
