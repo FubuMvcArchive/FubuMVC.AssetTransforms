71db04e3a2545c592efd0a72ea73ddcf734a1ea0
Alex Henning Johannessen <ahjohannessen@gmail.com>
2012-01-16 21:52:30 +0000

    Removed webactivator and added temporary solution for combination issue

--- src/HelloWorld/ConfigureFubuMVC.cs
+++ src/HelloWorld/ConfigureFubuMVC.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
+using System.Linq;
 using FubuMVC.Coffee;
 using FubuMVC.Core;
+using FubuMVC.Core.Assets.Combination;
 using FubuMVC.Less;
 using FubuMVC.Sass;
 using FubuMVC.Spark;
@@ -29,10 +32,31 @@ namespace HelloWorld
             Import<SassExtension>();
             Import<CoffeeExtension>();
 
-            Assets.CombineAllUniqueAssetRequests();
+            temporary();
 
             this.UseSpark();
             Views.TryToAttachWithDefaultConventions();
+        }
+
+
+
+        private void temporary()
+        {
+            // remove again when fix is applied for built-in
+            Services(s => s.SetServiceIfNone<ICombinationDeterminationService, TemporaryCombinationService>());
+        }
+    }
+
+    public class TemporaryCombinationService : CombinationDeterminationService
+    {
+        public TemporaryCombinationService(IAssetCombinationCache combinations)
+            : base(combinations, new List<ICombinationPolicy> { new CombineAllScriptFiles(), new CombineAllStylesheets() }) {}
+
+        public override void TryToReplaceWithCombinations(AssetTagPlan plan)
+        {
+            var mimeTypePolicies = policies.Where(x => x.MimeType == plan.MimeType);
+            mimeTypePolicies.Each(p => ExecutePolicy(plan, p));
+            base.TryToReplaceWithCombinations(plan);
         }
     }
 }
---
+++ src/HelloWorld/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using Bottles;
+using FubuMVC.Core;
+using FubuMVC.StructureMap;
+using StructureMap;
+
+namespace HelloWorld
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            FubuApplication.For<ConfigureFubuMVC>()
+                .StructureMap(new Container())
+                .Bootstrap();
+
+            PackageRegistry.AssertNoFailures();
+        }
+    }
+}
