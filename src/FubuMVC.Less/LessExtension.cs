4937f541e6b312a52ea91f24f7722210b334d7fe
Artem Smirnov <uluhonolulu@gmail.com>
2012-03-01 11:07:04 +0400

    Use AssetPathResolver in less minification in order to handle the @import directive properly.

--- src/FubuMVC.Less/LessExtension.cs
+++ src/FubuMVC.Less/LessExtension.cs
@@ -1,7 +1,12 @@
-﻿using FubuMVC.Core;
+using System.Web;
+using FubuMVC.Core;
 using FubuMVC.Core.Assets.Content;
 using FubuMVC.Core.Registration;
 using dotless.Core;
+using dotless.Core.Importers;
+using dotless.Core.Input;
+using dotless.Core.Parser;
+using dotless.Core.Stylizers;
 
 namespace FubuMVC.Less
 {
@@ -14,9 +19,15 @@ namespace FubuMVC.Less
 
         private static void services(IServiceRegistry s)
         {
-            s.SetServiceIfNone<ILessEngine>(new LessEngine());
+    		s.SetServiceIfNone<ILessEngine>(new LessEngine(new Parser(new PlainStylizer(), new Importer(new FileReader(new AssetPathResolver())))));
             s.SetServiceIfNone<ILessCompiler, LessCompiler>();
             s.AddService<ITransformerPolicy, LessTransformerPolicy>();
         }
+		internal class AssetPathResolver : IPathResolver {
+			public string GetFullPath(string path) {
+				return HttpContext.Current.Server.MapPath(path).Replace(@"\_content\", @"\Content\");
+			}
+		}
     }
 }
+
