7672e4c1f85572036049096c8f73ee464d0ec714
jeremydmiller <jeremydmiller@yahoo.com>
2012-12-31 08:19:50 -0600

    replaced the older SassAndCoffee coffeescript compiler in favor of Cassette

--- src/FubuMVC.Coffee/CoffeeExtension.cs
+++ src/FubuMVC.Coffee/CoffeeExtension.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using FubuMVC.Coffee.Compilers;
+﻿using FubuMVC.Coffee.Compilers;
 using FubuMVC.Core;
 using FubuMVC.Core.Assets.Content;
 using FubuMVC.Core.Registration;
-using FubuMVC.Core.Registration.ObjectGraph;
 using FubuMVC.Core.Runtime;
-using SassAndCoffee.Core;
-using SassAndCoffee.JavaScript;
-using SassAndCoffee.JavaScript.CoffeeScript;
 
 namespace FubuMVC.Coffee
 {
@@ -19,27 +14,17 @@ namespace FubuMVC.Coffee
 
         public void Configure(FubuRegistry registry)
         {
-            registry.Services(registerDefaultServices);
+            registry.Services<CoffeeServices>();
             MimeType.Javascript.AddExtension(COFFEE_EXTENSION);
-        }
-
-        private static void registerDefaultServices(ServiceRegistry registry)
-        {
-            registerCompiler(registry);
-            registry.AddService<ITransformerPolicy, CoffeeTransformerPolicy>();
-        }
-
-        private static void registerCompiler(ServiceRegistry registry)
-        {
-            var compilerDef = registry.SetServiceIfNone(typeof (ICoffeeCompiler), typeof (SassCoffeeCompiler));
-            var innerCompilerDef = ObjectDef.ForType<CoffeeScriptCompiler>();
-            
-            var instanceProvider = ObjectDef.ForType<InstanceProvider<IJavaScriptRuntime>>();
-            Func<IJavaScriptRuntime> runtimeFunc = () => new IEJavaScriptRuntime();
-            instanceProvider.DependencyByValue(typeof(Func<IJavaScriptRuntime>), runtimeFunc);
-            innerCompilerDef.Dependency(typeof(IInstanceProvider<IJavaScriptRuntime>), instanceProvider);
-            compilerDef.Dependency(typeof(CoffeeScriptCompiler), innerCompilerDef);
         }
     }
 
+    public class CoffeeServices : ServiceRegistry
+    {
+        public CoffeeServices()
+        {
+            AddService<ITransformerPolicy, CoffeeTransformerPolicy>();
+            SetServiceIfNone<ICoffeeCompiler, CoffeeCompiler>();
+        }
+    }
 }
--- src/FubuMVC.Coffee/Compilers/ICoffeeCompiler.cs
+++ src/FubuMVC.Coffee/Compilers/ICoffeeCompiler.cs
@@ -1,7 +1,21 @@
+using Cassette;
+using Cassette.Scripts;
+
 namespace FubuMVC.Coffee.Compilers
 {
     public interface ICoffeeCompiler
     {
         string Compile(string code);
+    }
+
+    public class CoffeeCompiler : ICoffeeCompiler
+    {
+        public string Compile(string code)
+        {
+            return new JurassicCoffeeScriptCompiler().Compile(code, new CompileContext
+            {
+                SourceFilePath = "unknown"
+            }).Output;
+        }
     }
 }
